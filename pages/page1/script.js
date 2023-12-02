5cfd52ad8a393b674f2c02bd8a509c4b7d6f89d5
git1bhay <soniabhay797@gmail.com>
2023-12-02 12:13:46 +0530

    update

--- pages/page1/script.js
+++ pages/page1/script.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 // import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js';
-// import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer';
+// import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js';
 
 import {Water} from 'three/examples/jsm/objects/Water';
 
@@ -10,13 +9,17 @@ const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight, 0.1,1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
+
 document.body.appendChild(renderer.domElement);
+
 
 
 const pointLight = new THREE.PointLight(0xffffff,100,500);
 pointLight.position.set(0,3,0)
 
 scene.add(pointLight);
+
+
 
 const watergeometry = new THREE.PlaneGeometry(20000,20000);
 
@@ -29,15 +32,14 @@ const water = new Water(watergeometry,{
   }),
 
   sunDirection: pointLight.position,
-  sunColor: 0x87CEEB,
-  waterColor: 0x87CEEB,
+  sunColor: 0x87CEE8,
+  waterColor: 0x00546A,
   distortionScale:4
 
 
 })
 
 water.position.y = 1
-// const waterhelper = new water
 
 
 water.rotation.x = Math.PI*(-0.5);
@@ -50,15 +52,26 @@ function update(){
   requestAnimationFrame(update)
 }
 
+const contentElement = document.querySelector('.content');
+
+const movementSpeed = 0.1;
+
+document.addEventListener('mousemove', onMouseMove);
+
+function onMouseMove(event) {
+  const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+
+  contentElement.style.transform = `translateX(${mouseX * movementSpeed * 250}px)`;
+}
+
 
 
 // const controls = new OrbitControls(camera,renderer.domElement);
-// controls.enablePan = fals
 
-// const loader = new THREE.TextureLoader();
+const loader = new THREE.TextureLoader();
 
-// scene.background = loader.load('./images/sky.jpg');
-scene.background = new THREE.Color(0.1,0.5,0.5);
+scene.background = loader.load('./images/sea.jpg');
+// scene.background = new THREE.Color(0.1,0.5,0.5);
 
 
 
@@ -69,59 +82,49 @@ window.addEventListener('resize',function(){
 })
 
 
-const flyControls = new FlyControls(camera, renderer.domElement);
 
-// camera.position.set(0, 10, 50);
-flyControls.movementSpeed = 0.05;
-
-flyControls.autoForward = false;
-flyControls.dragToLook = false;
+camera.position.y = 15;
 
 
-camera.position.y = 55;
+camera.position.z = 50;
 
-
-
-
-camera.lookAt(-2.5,32,23);
+camera.lookAt(288,42,48);
 
 // const camerahelper = new THREE.CameraHelper(camera);
 // scene.add(camerahelper);
 // camerahelper.scale.set(50,50,50);
 
 
-// const cssRenderer = new CSS3DRenderer();
-// cssRenderer.setSize(window.innerWidth, window.innerHeight);
-// cssRenderer.domElement.style.position = 'absolute';
-// cssRenderer.domElement.style.top = 0;
-// document.body.appendChild(cssRenderer.domElement);
 
-// // Create a HTML element
-// const element = document.createElement('div');
-// element.style.width = '10px';
-// element.style.height = '25px';
-// element.style.background = 'red';
-// element.style.color = 'black';
-// element.textContent = 'Flying HTML!';
-// const cssObject = new CSS3DObject(element);
-// cssObject.position.set(5, 3, 20); 
-// cssObject.rotation.set(0,10,-20)
-// scene.add(cssObject);
 
-const gridHelper = new THREE.GridHelper(200, 10);
-scene.add(gridHelper);
+// const gridHelper = new THREE.GridHelper(200, 10);
+// scene.add(gridHelper);
 
+
+let mouseX = 0
+
+let windowHalfX = window.innerWidth / 2;
+
+document.addEventListener( 'mousemove', onDocumentMouseMove );
+
+function onDocumentMouseMove( event ) {
+
+  mouseX = ( event.clientX - windowHalfX ) * 0.1;
+
+}
 
 
 animate()
 
 function animate(){
   // controls.update();
-  flyControls.update(0.01);
+
+  camera.position.x += ( mouseX - camera.position.x ) * .05;
+	camera.lookAt( scene.position );
+
   renderer.render(scene,camera);
   requestAnimationFrame(animate);
-  // cssRenderer.render(scene, camera);
   // camera.position.z += (mouseX - camera.position.z)*0.002;
-  // console.log(camera.position)
+  console.log(camera.position)
 }
 
