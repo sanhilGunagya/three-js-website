4bb5ad65b163ab9f81afa3216507f11f20b665e7
sanhilGunagya <sanhil.rai@gunagya.in>
2023-12-02 12:32:10 +0530

    up

--- pages/page1/script.js
+++ pages/page1/script.js
@@ -1,72 +1,129 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { Water } from 'three/examples/jsm/objects/Water';
+// import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+// import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js';
 
-// Three.js setup
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+import {Water} from 'three/examples/jsm/objects/Water';
+
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight, 0.1,1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth,window.innerHeight);
 
-// Append the renderer to the content div
 document.body.appendChild(renderer.domElement);
 
-// Light, water, and other Three.js elements
-const pointLight = new THREE.PointLight(0xffffff, 100, 500);
-pointLight.position.set(0, 3, 0);
+
+
+const pointLight = new THREE.PointLight(0xffffff,100,500);
+pointLight.position.set(0,3,0)
+
 scene.add(pointLight);
 
-const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
 
-const water = new Water(waterGeometry, {
+
+const watergeometry = new THREE.PlaneGeometry(20000,20000);
+
+
+const water = new Water(watergeometry,{
   textureHeight: 512,
   textureWidth: 512,
-  waterNormals: new THREE.TextureLoader().load('images/waternormals.jpg', function (texture) {
+  waterNormals: new THREE.TextureLoader().load('images/waternormals.jpg',function(texture){
     texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
   }),
-  sunDirection: pointLight.position,
-  sunColor: 0x87CEEB,
-  waterColor: 0x00546A,
-  distortionScale: 4,
-});
 
-water.position.y = 1;
-water.rotation.x = Math.PI * -0.5;
+  sunDirection: pointLight.position,
+  sunColor: 0x87CEE8,
+  waterColor: 0x00546A,
+  distortionScale:4
+
+
+})
+
+water.position.y = 1
+
+
+water.rotation.x = Math.PI*(-0.5);
 scene.add(water);
 
-// Your content in the HTML file
+
+update()
+function update(){
+  water.material.uniforms['time'].value += 0.01;
+  requestAnimationFrame(update)
+}
+
 const contentElement = document.querySelector('.content');
 
-// Variable to control content movement
 const movementSpeed = 0.1;
 
-// Mouse movement event listener
 document.addEventListener('mousemove', onMouseMove);
 
 function onMouseMove(event) {
   const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-  // Move content based on mouse movement
-  contentElement.style.transform = `translateX(${mouseX * movementSpeed * 100}px)`;
+
+  contentElement.style.transform = `translateX(${mouseX * movementSpeed * 250}px)`;
+}
+
+
+
+// const controls = new OrbitControls(camera,renderer.domElement);
+
+const loader = new THREE.TextureLoader();
+
+scene.background = loader.load('./images/sea.jpg');
+// scene.background = new THREE.Color(0.1,0.5,0.5);
+
+
+
+window.addEventListener('resize',function(){
+  camera.aspect = window.innerWidth/window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth,window.innerHeight);
+})
+
+
+
+camera.position.y = 15;
+
+
+camera.position.z = 50;
+
+camera.lookAt(288,42,48);
+
+// const camerahelper = new THREE.CameraHelper(camera);
+// scene.add(camerahelper);
+// camerahelper.scale.set(50,50,50);
+
+
+
+
+// const gridHelper = new THREE.GridHelper(200, 10);
+// scene.add(gridHelper);
+
+
+let mouseX = 0
+
+let windowHalfX = window.innerWidth / 2;
+
+document.addEventListener( 'mousemove', onDocumentMouseMove );
+
+function onDocumentMouseMove( event ) {
+
+  mouseX = ( event.clientX - windowHalfX ) * 0.1;
 
 }
 
-// Orbit controls setup
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enablePan = false;
-controls.enableZoom = false;
 
-// Set background color
-scene.background = new THREE.Color(0.1, 0.5, 0.5);
+animate()
 
-// Set initial camera position
-camera.position.set(-2.5, 32, 23);
+function animate(){
+  // controls.update();
 
-// Animation update function
-function update() {
-  water.material.uniforms['time'].value += 0.01;
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(update);
+  camera.position.x += ( mouseX - camera.position.x ) * .05;
+	camera.lookAt( scene.position );
+
+  renderer.render(scene,camera);
+  requestAnimationFrame(animate);
+  // camera.position.z += (mouseX - camera.position.z)*0.002;
+  console.log(camera.position)
 }
-
-update();
