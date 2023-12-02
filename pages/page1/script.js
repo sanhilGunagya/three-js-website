01cadf5f238e3c579739625ab728940bf4606736
git1bhay <soniabhay797@gmail.com>
2023-12-02 19:12:24 +0530

    abhay code

--- pages/page1/script.js
+++ pages/page1/script.js
@@ -1,40 +1,41 @@
 import * as THREE from 'three';
-// import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js';
 
-import {Water} from 'three/examples/jsm/objects/Water';
+import { Water } from 'three/examples/jsm/objects/Water';
 
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight, 0.1,1000);
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
 
 
-const pointLight = new THREE.PointLight(0xffffff,100,500);
-pointLight.position.set(0,3,0)
+const pointLight = new THREE.PointLight(0xffffff, 100, 500);
+pointLight.position.set(0, 3, 0)
 
 scene.add(pointLight);
 
 
 
-const watergeometry = new THREE.PlaneGeometry(20000,20000);
+const watergeometry = new THREE.PlaneGeometry(20000, 20000);
 
 
-const water = new Water(watergeometry,{
+const water = new Water(watergeometry, {
   textureHeight: 512,
   textureWidth: 512,
-  waterNormals: new THREE.TextureLoader().load('images/waternormals.jpg',function(texture){
+  waterNormals: new THREE.TextureLoader().load('images/waternormals.jpg', function (texture) {
     texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
   }),
 
   sunDirection: pointLight.position,
   sunColor: 0x87CEE8,
   waterColor: 0x00546A,
-  distortionScale:4
+  distortionScale: 4
 
 
 })
@@ -42,12 +43,12 @@ const water = new Water(watergeometry,{
 water.position.y = 1
 
 
-water.rotation.x = Math.PI*(-0.5);
+water.rotation.x = Math.PI * (-0.5);
 scene.add(water);
 
 
 update()
-function update(){
+function update() {
   water.material.uniforms['time'].value += 0.01;
   requestAnimationFrame(update)
 }
@@ -66,7 +67,7 @@ function onMouseMove(event) {
 
 
 
-// const controls = new OrbitControls(camera,renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const loader = new THREE.TextureLoader();
 
@@ -75,10 +76,10 @@ scene.background = loader.load('./images/sea.jpg');
 
 
 
-window.addEventListener('resize',function(){
-  camera.aspect = window.innerWidth/window.innerHeight;
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth,window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 })
 
 
@@ -88,7 +89,7 @@ camera.position.y = 15;
 
 camera.position.z = 50;
 
-camera.lookAt(288,42,48);
+camera.lookAt(288, 42, 48);
 
 // const camerahelper = new THREE.CameraHelper(camera);
 // scene.add(camerahelper);
@@ -100,31 +101,88 @@ camera.lookAt(288,42,48);
 // const gridHelper = new THREE.GridHelper(200, 10);
 // scene.add(gridHelper);
 
+const loader1 = new GLTFLoader();
 
+loader1.load('dolphin/scene.gltf', (gltf, err) => {
+  if (err) {
+    console.log(err)
+  }
+  const model = gltf.scene;
+  model.scale.set(25, 25, 25);
+  model.position.z = -55;
+  scene.add(model)
+  const light = new THREE.DirectionalLight(0xffffff, 1)
+  scene.add(light)
+
+  const mixer = new THREE.AnimationMixer(model);
+  const action = mixer.clipAction(gltf.animations[0])
+  action.play()
+
+  // let angle = 40;
+  // const radius = 20;
+  animation()
+  var a = 35; 
+  function animation() {
+    mixer.update(0.01)
+    
+    // model.position.z += 0.1;
+    
+    
+    if(model.position.y<a){
+        model.position.y += 0.2;
+      if(a===34){
+        a = -1000
+      }
+    }
+    else{
+      
+      // model.position.y = 1
+      model.position.y = model.position.y - 0.2;
+      console.log(model.position.y)
+    }
+    
+    
+    
+  
+
+    // model.rotation.x += 0.01;
+    // model.rotation.x -= 0.02
+    // model.rotation.z += 0.01;
+    
+      // console.log(model.position.y)
+      requestAnimationFrame(animation);
+  }
+
+  // move()
+  // function move(){
+  //   model.position.y += 0.5;
+  //   requestAnimationFrame(move)
+  // }
+
+})
 let mouseX = 0
 
 let windowHalfX = window.innerWidth / 2;
 
-document.addEventListener( 'mousemove', onDocumentMouseMove );
+document.addEventListener('mousemove', onDocumentMouseMove);
 
-function onDocumentMouseMove( event ) {
+function onDocumentMouseMove(event) {
 
-  mouseX = ( event.clientX - windowHalfX ) * 0.1;
+  mouseX = (event.clientX - windowHalfX) * 0.1;
 
 }
 
 
 animate()
 
-function animate(){
-  // controls.update();
+function animate() {
+  controls.update();
 
-  camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.lookAt( scene.position );
-
-  renderer.render(scene,camera);
-  requestAnimationFrame(animate);
+  // camera.position.x += ( mouseX - camera.position.x ) * .05;
+  camera.lookAt(scene.position);
+  requestAnimationFrame(animate)
+  renderer.render(scene,camera)
   // camera.position.z += (mouseX - camera.position.z)*0.002;
-  console.log(camera.position)
+  // console.log(camera.position)
 }
 
--- pages/page1/script1.js
+++ pages/page1/script1.js
@@ -2,7 +2,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const scene = new THREE.Scene();
 
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
 
 const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000);
 // camera.position.set(-2057, -1560, -106);
@@ -13,86 +14,67 @@ document.body.appendChild(renderer.domElement);
 
 let controls = new OrbitControls(camera, renderer.domElement);
 // controls.addEventListener('change', renderer);
-controls.minDistance = 50;
-// controls.maxDistance = 10000;
+controls.minDistance = 5000;
+controls.maxDistance = 12000;
+controls.minPolarAngle = Math.PI / 2.5;
+controls.maxPolarAngle = Math.PI / 2.5;
+controls.enableDamping = true; // 
 
-// camera.lookAt(-5057, -1060, -2000);
-// camera.position.z = 50;
-
-
-// let materialArray = [];
-// let texture_1 = new THREE.TextureLoader().load('boximg/arid2_ft.jpg');
-// let texture_2 = new THREE.TextureLoader().load('boximg/arid2_bk.jpg');
-// let texture_3 = new THREE.TextureLoader().load('boximg/arid2_up.jpg');
-// let texture_4 = new THREE.TextureLoader().load('boximg/arid2_dn.jpg');
-// let texture_5 = new THREE.TextureLoader().load('boximg/arid2_rt.jpg');
-// let texture_6 = new THREE.TextureLoader().load('boximg/arid2_lf.jpg');
-
-// materialArray.push(new THREE.MeshBasicMaterial({ map: texture_1 }));
-// materialArray.push(new THREE.MeshBasicMaterial({ map: texture_2 }));
-// materialArray.push(new THREE.MeshBasicMaterial({ map: texture_3 }));
-// materialArray.push(new THREE.MeshBasicMaterial({ map: texture_4 }));
-// materialArray.push(new THREE.MeshBasicMaterial({ map: texture_5 }));
-// materialArray.push(new THREE.MeshBasicMaterial({ map: texture_6 }));
-
-// for (let i = 0; i < 6; i++)
-//     materialArray[i].side = THREE.BackSide;
-
-// let skyboxGeo = new THREE.BoxGeometry(15000, 15000, 15000);
-// let skybox = new THREE.Mesh(skyboxGeo, materialArray);
-// scene.add(skybox);
 
 const loader = new GLTFLoader();
 
-loader.load('model/scene.gltf',(gltf,err)=>{
-    if(err){
+loader.load('model/scene.gltf', (gltf, err) => {
+    if (err) {
         console.log(err)
     }
     const model = gltf.scene;
     scene.add(model)
-    model.scale.set(10,10,10)
-    model.position.set(0,-20000,0)
-    model.traverse( function ( child ) {
+    model.traverse(function (child) {
 
-        if ( child.isMesh ) {
+        if (child.isMesh) {
 
             var textureLoader = new THREE.TextureLoader();
-            var texture = textureLoader.load( 'model/seatexture.jpg' );
+            var texture = textureLoader.load('model/seatexture.jpg');
 
-            var material = new THREE.MeshStandardMaterial( { map: texture } );
+            var material = new THREE.MeshStandardMaterial({ map: texture });
             child.material = material;
 
         }
+    })
 })
-})
+
+// const loader1 = new GLTFLoader()
+// loader1.lo
 const texture = new THREE.TextureLoader().load('images/seaimg.jpg');
 scene.background = texture;
 
-// const fog = new THREE.Fog(0xffffff,1,100)
-// scene.fog = fog
 
-const light = new THREE.AmbientLight(0xffffff,2);
+
+const light = new THREE.AmbientLight(0xffffff, 2);
 scene.add(light)
 
-camera.position.set(0,-110,-60)
-// camera.lookAt(0,-20000,0)
-// camera.lookAt(933659,10623,-102641)
-// camera.position.z = 55;
+camera.position.set(-8338, 1958, 10380)
 
 // const helper = new THREE.CameraHelper(camera);
 // scene.add(helper)
 // const gridhelper = new THREE.GridHelper(1000,10)
 // scene.add(gridhelper)
 
+
+
 const axishelper = new THREE.AxesHelper(500);
 scene.add(axishelper)
 
+const light1 = new THREE.DirectionalLight(0xffffff, 1);
+scene.add(light1)
+
+
 animate()
-function animate(){
+function animate() {
     controls.update();
-    renderer.render(scene,camera);
-    // scene.fog.near += 0.1;
-    // scene.fog.far += 0.1;
+    renderer.render(scene, camera);
+
+
     requestAnimationFrame(animate)
     // console.log(camera.position)
 }
